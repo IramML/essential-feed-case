77b7931a1789f31fe1ff6f0e7443576fe94ad1bb
IramML <irammartinezlamas@gmail.com>
2023-06-17 23:52:30 -0600

    Add test `LocalFeedLoader` does not deliver insertion error when instance is dealocated

--- EssentialFeed/EssentialFeedTests/Feed/Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Cache/CacheFeedUseCaseTests.swift
@@ -75,6 +75,20 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
         XCTAssertTrue(receivedResults.isEmpty)
     }
+    
+    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+
+        var receivedResults = [LocalFeedLoader.SaveResult]()
+        sut?.save(uniqueImageFeed().models) { receivedResults.append($0) }
+
+        store.completeDeletionSuccessfully()
+        sut = nil
+        store.completeInsertion(with: anyNSError())
+
+        XCTAssertTrue(receivedResults.isEmpty)
+    }
 
     // MARK: - Helpers
 
