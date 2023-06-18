5510f41d908be989f822e4f1e3d26b54e850450d
IramML <irammartinezlamas@gmail.com>
2023-06-17 19:10:37 -0600

    Add test cancel get from URL task

--- EssentialFeed/EssentialFeed/Shared/API Infra/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Shared/API Infra/URLSessionHTTPClient.swift
@@ -19,13 +19,17 @@ public final class URLSessionHTTPClient: HTTPClient {
     private struct URLSessionTaskWrapper: HTTPClientTask {
         let wrapped: URLSessionTask
         
-        func cancel() {}
+        func cancel() {
+            wrapped.cancel()
+        }
     }
 
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
         let task = session.dataTask(with: url) { data, response, error in
             completion(Result {
-                if let data = data, let response = response as? HTTPURLResponse {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
                     return (data, response)
                 }
                 
