bf04115dfe4cfbeadbffd4b67b1d155a4c5ac039
IramML <irammartinezlamas@gmail.com>
2023-06-18 11:22:25 -0600

    Add test `LocalFeedLoader` does not deliver items when cache expires

--- EssentialFeed/EssentialFeedTests/Feed/Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -50,6 +50,17 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
             store.completeRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
         })
     }
+    
+    func test_load_deliversNoImagesOnCacheExpiration() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let expirationTimestamp = fixedCurrentDate.minusFeedCacheMaxAge()
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: expirationTimestamp)
+        })
+    }
 
     // MARK: - Helpers
 
