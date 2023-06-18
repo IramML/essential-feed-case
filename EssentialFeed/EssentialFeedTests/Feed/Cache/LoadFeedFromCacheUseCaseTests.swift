2b7da3352a385dae2eb958cad8c8207f7f61c7c7
IramML <irammartinezlamas@gmail.com>
2023-06-18 11:25:20 -0600

    Add test `LocalFeedLoader` has no side effects on non expired cache

--- EssentialFeed/EssentialFeedTests/Feed/Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -90,6 +90,18 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
+    
+    func test_load_hasNoSideEffectsOnNonExpiredCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let nonExpiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        sut.load { _ in }
+        store.completeRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
+
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 
     // MARK: - Helpers
 
