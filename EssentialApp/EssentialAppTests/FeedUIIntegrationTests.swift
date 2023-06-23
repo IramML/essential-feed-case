49b74ed0eeea6f15a53311704afb27b18986e21d
IramML <irammartinezlamas@gmail.com>
2023-06-22 23:44:44 -0600

    Add test load feed completion dispatches from background to main thread

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -97,6 +97,18 @@ class FeedUIIntegrationTests: XCTestCase {
         loader.completeFeedLoading(with: [], at: 1)
         assertThat(sut, isRendering: [])
     }
+    
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
 
     // MARK: - Helpers
 
