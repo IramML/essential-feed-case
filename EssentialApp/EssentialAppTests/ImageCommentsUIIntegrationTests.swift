9f592c9460665cf8a6e2f93a043c12285282f37e
IramML <irammartinezlamas@gmail.com>
2023-06-23 00:15:37 -0600

    Add test image comments hides error when tap on error message

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -118,6 +118,19 @@ final class ImageCommentsUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, nil)
     }
     
+    func test_tapOnErrorView_hidesErrorMessage() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.errorMessage, nil)
+
+        loader.completeCommentsLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, loadError)
+
+        sut.simulateErrorViewTap()
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(
