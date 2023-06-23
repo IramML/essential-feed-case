f1a9fdd66fc867a359b03847aea32f3b52c7e983
IramML <irammartinezlamas@gmail.com>
2023-06-22 23:28:45 -0600

    Add test feed has title

---
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialAppTests
+//
+//  Created by Iram Martinez on 22/06/23.
+//
+
+import EssentialFeed
+
+var feedTitle: String {
+    FeedPresenter.title
+}
